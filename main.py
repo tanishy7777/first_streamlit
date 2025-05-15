08c6f1d1aa8babca7d7b69710491459ad88fffc1
Tanish Yelgoe <143334319+tanishy7777@users.noreply.github.com>
2025-05-15 20:02:48 +0530

    init

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+from openai import OpenAI
 import streamlit as st
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+client = OpenAI(
+    api_key=st.secrets["GEMINI_API_KEY"],
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+
+st.title("ChatGPT-like clone")
+
+if "gemini_model" not in st.session_state:
+    st.session_state["gemini_model"] = "gemini-2.5-flash"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model="gemini-2.0-flash",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+                stream=True
+            )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
